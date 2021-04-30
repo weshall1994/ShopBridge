4fe97a8b8ee41ed9f904c2a525c0792bb85d856b
vishal ghadage <vishal@delighteck.com>
2021-05-01 01:24:55 +0530

    logo color opacity added.

--- shop_bridge_app/src/LoginForm.jsx
+++ shop_bridge_app/src/LoginForm.jsx
@@ -23,10 +23,10 @@ function LoginForm(Props) {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <h2 className="mb-2 text-center text-2xl font-medium text-indigo-600 justify-self-center mx-auto">
-              Login
+              SHOP BRIDGE
             </h2>
             <div className="flex justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 bg-yellow-300 rounded-lg text-blue-400" viewBox="0 0 20 20" fill="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 py-3 bg-yellow-300 rounded-lg text-indigo-600 bg-opacity-100" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
               </svg>
             </div>
