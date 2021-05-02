8a74ea1c9522b66b5d6f8b43c892df20f1373742
vishal ghadage <vishal@delighteck.com>
2021-05-02 13:03:20 +0530

    customer dashboard header added.

---
+++ shop_bridge_app/src/Components/Dashboard/Components/CustomerComponents/CustHeaderCard.jsx
@@ -0,0 +1,39 @@
+import { CurrencyRupeeIcon } from '@heroicons/react/outline'
+import React, { useEffect, useState } from 'react'
+
+function CustHeaderCard(props) {
+  console.log("props", props)
+
+  let i = 0;
+
+  return (
+    <div>
+      <div className="max-w-md py-4 mx-4 px-8 bg-white shadow-lg rounded-lg my-16">
+        {props.category &&
+          <>
+            {props.category[i] &&
+              <>
+                <div className="flex justify-center md:justify-end -mr-5 -mt-16">
+                  <img className="h-28 w-28 rounded-full border-2 border-gray-200 shadow-xl" src={props.category[i].image} />
+                </div>
+                <div>
+                  <h2 className="text-gray-800 text-sm mt-5 font-bold overflow-none truncate">{i + " " + props.category[i].title}</h2>
+                </div>
+                <div className={"flex justify-center md:justify-end"}>
+                  <CurrencyRupeeIcon className="h-4 w-4 text-gray-500 mt-3" />
+                  <p className="mt-2 text-gray-600 justify-center"> {props.category[i].price}</p>
+                </div>
+              </>
+            }
+          </>
+        }
+
+        <div className="flex justify-end mt-4">
+          <a href="#" className=" font-bold uppercase text-indigo-500">{props.categoryname}</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CustHeaderCard
--- shop_bridge_app/src/Components/Dashboard/CustomerIndex.jsx
+++ shop_bridge_app/src/Components/Dashboard/CustomerIndex.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import HocGetAllProducts from '../../Common/Componets/HocGetAllProducts'
+import CustHeaderCard from './Components/CustomerComponents/CustHeaderCard';
 
 function CustomerIndex(props) {
+  const { products, categoryNames } = props;
   return (
-    <div className="divide-y divide-yellow-500">
-      <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+    <div>
+      {products &&
+        <div className={`grid lg:grid-cols-4 gap-1 md:grid-cols-3 sm:grid-cols-2 bg-blue-500 w-full h-56 shadow-lg`}>
+          {categoryNames &&
+            <>
+              {
+                categoryNames.length > 0 &&
+                <>
+                  {
+                    categoryNames.map((c, index) => (
+                      <CustHeaderCard key={index}
+                        categoryname={c}
+                        category={products.filter(p => p.category == c)}
+                        length={products.filter(p => p.category == c).length} />
+                    ))
+                  }
+                </>
+              }
+            </>
+
+          }
+        </div>
+      }
     </div>
   )
 }
